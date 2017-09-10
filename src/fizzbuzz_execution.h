a68034bfc202f56460446164d30f0b4a39b13331
David Cordero Chavarría <dacoch215@estudiantec.cr>
2017-09-10 13:20:14 -0600

    Separate fizzbuzz into separate units

---
+++ src/fizzbuzz_execution.h
@@ -0,0 +1,9 @@
+#ifndef FIZZBUZZ_EXECUTION_H
+#define FIZZBUZZ_EXECUTION_H
+
+#include "helper_functions.h"
+
+void
+run_fizzbuzz_until_with_buffer ( int number, char * buffer );
+
+#endif /* FIZZBUZZ_EXECUTION_H */
---
+++ src/helper_functions.h
@@ -0,0 +1,33 @@
+#ifndef HELPER_FUNCTIONS_H
+#define HELPER_FUNCTIONS_H
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+#ifdef SLEEP
+#include <unistd.h>  /* sleep (). */
+
+void
+u_sleep ( int microseconds );
+#endif /* SLEEP */
+
+bool
+is_first_divisible_by_second ( int first, int second );
+
+bool
+nor ( bool expr_A, bool expr_B );
+
+void
+append_fizz_to_char_buffer ( char * buffer );
+
+void
+append_buzz_to_buffer ( char * buffer );
+
+void
+set_buffer_to_number ( char * buffer, int * number_ptr );
+
+void
+set_fizzbuzz ( int * number_ptr, char * message_buffer );
+
+#endif /* HELPER_FUNCTIONS_H */
