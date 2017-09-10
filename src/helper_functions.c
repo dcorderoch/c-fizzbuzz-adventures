1d1ee6c2ad215187f8da0617a9478a261877f39e
David Cordero Chavarría <dacoch215@estudiantec.cr>
2017-09-10 13:38:54 -0600

    Bugfix: fizzbuzz printed on wrong numbers

--- src/helper_functions.c
+++ src/helper_functions.c
@@ -4,6 +4,10 @@
 #define BUZZ_NUMBER 5
 
 #ifdef SLEEP
+#define DELAY_IN_MILLISECONDS 75
+#endif
+
+#ifdef SLEEP
 void
 u_sleep ( int microseconds )
 {
@@ -47,10 +51,25 @@ set_fizzbuzz ( int * number_ptr, char * message_buffer )
   bool is_fizz = is_first_divisible_by_second ( *number_ptr, FIZZ_NUMBER );
   bool is_buzz = is_first_divisible_by_second ( *number_ptr, BUZZ_NUMBER );
 
+  int buf_len = strlen ( message_buffer );
+  memset ( message_buffer, 0, buf_len );
+
   if ( is_fizz )
     append_fizz_to_char_buffer ( message_buffer );
   if ( is_buzz )
     append_buzz_to_buffer ( message_buffer );
   if ( nor ( is_fizz, is_buzz ) )
     set_buffer_to_number ( message_buffer, number_ptr );
+}
+
+void
+print_fizzbuzz ( int * number, char * message_buffer )
+{
+  #ifdef NUMBER
+  printf ( "%03d - ", *number );
+  #endif
+  printf ( "%s\n", message_buffer );
+  #ifdef SLEEP
+  u_sleep ( DELAY_IN_MILLISECONDS );
+  #endif
 }
